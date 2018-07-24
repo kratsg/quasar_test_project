9fb4e12a2502e2b2dfe00fffbbbe4c8c356aecb1
Giordon Stark <kratsg@gmail.com>
2018-07-24 12:41:41 -0500

    update using revital's example Design/config

--- AddressSpace/cmake_generated.cmake
+++ AddressSpace/cmake_generated.cmake
@@ -1,10 +1,106 @@
 
+
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/AddressSpace/include/ASGFEXBoard.h ${PROJECT_BINARY_DIR}/AddressSpace/src/ASGFEXBoard.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate asclass GFEXBoard --project_binary_dir  ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassHeader.xslt ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassBody.xslt Configuration.hxx_GENERATED validateDesign
+	)	
+	
+	
+
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/AddressSpace/include/ASFunctionality.h ${PROJECT_BINARY_DIR}/AddressSpace/src/ASFunctionality.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate asclass Functionality --project_binary_dir  ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassHeader.xslt ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassBody.xslt Configuration.hxx_GENERATED validateDesign
+	)	
+	
+	
+
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/AddressSpace/include/ASPowerManagementChip.h ${PROJECT_BINARY_DIR}/AddressSpace/src/ASPowerManagementChip.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate asclass PowerManagementChip --project_binary_dir  ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassHeader.xslt ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassBody.xslt Configuration.hxx_GENERATED validateDesign
+	)	
+	
+	
+
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/AddressSpace/include/ASBoardTemperatureSensors.h ${PROJECT_BINARY_DIR}/AddressSpace/src/ASBoardTemperatureSensors.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate asclass BoardTemperatureSensors --project_binary_dir  ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassHeader.xslt ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassBody.xslt Configuration.hxx_GENERATED validateDesign
+	)	
+	
+	
+
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/AddressSpace/include/ASDcDcConverter.h ${PROJECT_BINARY_DIR}/AddressSpace/src/ASDcDcConverter.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate asclass DcDcConverter --project_binary_dir  ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassHeader.xslt ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassBody.xslt Configuration.hxx_GENERATED validateDesign
+	)	
+	
+	
+
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/AddressSpace/include/ASBoardHealthFlags.h ${PROJECT_BINARY_DIR}/AddressSpace/src/ASBoardHealthFlags.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate asclass BoardHealthFlags --project_binary_dir  ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassHeader.xslt ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassBody.xslt Configuration.hxx_GENERATED validateDesign
+	)	
+	
+	
+
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/AddressSpace/include/ASMiniPODs.h ${PROJECT_BINARY_DIR}/AddressSpace/src/ASMiniPODs.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate asclass MiniPODs --project_binary_dir  ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassHeader.xslt ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassBody.xslt Configuration.hxx_GENERATED validateDesign
+	)	
+	
+	
+
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/AddressSpace/include/ASPowerModule.h ${PROJECT_BINARY_DIR}/AddressSpace/src/ASPowerModule.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate asclass PowerModule --project_binary_dir  ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassHeader.xslt ${PROJECT_SOURCE_DIR}/AddressSpace/designToClassBody.xslt Configuration.hxx_GENERATED validateDesign
+	)	
+	
+	
 	
 	set(ADDRESSSPACE_CLASSES 
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/src/ASGFEXBoard.cpp
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/src/ASFunctionality.cpp
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/src/ASPowerManagementChip.cpp
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/src/ASBoardTemperatureSensors.cpp
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/src/ASDcDcConverter.cpp
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/src/ASBoardHealthFlags.cpp
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/src/ASMiniPODs.cpp
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/src/ASPowerModule.cpp
 	
 	)
 	
 	set(ADDRESSSPACE_HEADERS
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/include/ASGFEXBoard.h
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/include/ASFunctionality.h
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/include/ASPowerManagementChip.h
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/include/ASBoardTemperatureSensors.h
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/include/ASDcDcConverter.h
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/include/ASBoardHealthFlags.h
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/include/ASMiniPODs.h
+	
+	${PROJECT_BINARY_DIR}/AddressSpace/include/ASPowerModule.h
 	
 	)
 	
--- Device/generated/cmake_header.cmake
+++ Device/generated/cmake_header.cmake
@@ -1,12 +1,94 @@
 
 	
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/Device/generated/Base_DGFEXBoard.h ${PROJECT_BINARY_DIR}/Device/generated/Base_DGFEXBoard.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate base GFEXBoard --project_binary_dir ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/quasar.py ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseHeader.xslt Configuration.hxx_GENERATED validateDesign ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseBody.xslt
+	)	
+	
+	
+	
+	
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/Device/generated/Base_DFunctionality.h ${PROJECT_BINARY_DIR}/Device/generated/Base_DFunctionality.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate base Functionality --project_binary_dir ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/quasar.py ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseHeader.xslt Configuration.hxx_GENERATED validateDesign ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseBody.xslt
+	)	
+	
+	
+	
+	
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/Device/generated/Base_DPowerManagementChip.h ${PROJECT_BINARY_DIR}/Device/generated/Base_DPowerManagementChip.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate base PowerManagementChip --project_binary_dir ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/quasar.py ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseHeader.xslt Configuration.hxx_GENERATED validateDesign ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseBody.xslt
+	)	
+	
+	
+	
+	
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/Device/generated/Base_DBoardTemperatureSensors.h ${PROJECT_BINARY_DIR}/Device/generated/Base_DBoardTemperatureSensors.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate base BoardTemperatureSensors --project_binary_dir ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/quasar.py ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseHeader.xslt Configuration.hxx_GENERATED validateDesign ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseBody.xslt
+	)	
+	
+	
+	
+	
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/Device/generated/Base_DDcDcConverter.h ${PROJECT_BINARY_DIR}/Device/generated/Base_DDcDcConverter.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate base DcDcConverter --project_binary_dir ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/quasar.py ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseHeader.xslt Configuration.hxx_GENERATED validateDesign ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseBody.xslt
+	)	
+	
+	
+	
+	
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/Device/generated/Base_DBoardHealthFlags.h ${PROJECT_BINARY_DIR}/Device/generated/Base_DBoardHealthFlags.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate base BoardHealthFlags --project_binary_dir ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/quasar.py ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseHeader.xslt Configuration.hxx_GENERATED validateDesign ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseBody.xslt
+	)	
+	
+	
+	
+	
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/Device/generated/Base_DMiniPODs.h ${PROJECT_BINARY_DIR}/Device/generated/Base_DMiniPODs.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate base MiniPODs --project_binary_dir ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/quasar.py ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseHeader.xslt Configuration.hxx_GENERATED validateDesign ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseBody.xslt
+	)	
+	
+	
+	
+	
+	add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/Device/generated/Base_DPowerModule.h ${PROJECT_BINARY_DIR}/Device/generated/Base_DPowerModule.cpp 
+	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	COMMAND python quasar.py generate base PowerModule --project_binary_dir ${PROJECT_BINARY_DIR}
+	DEPENDS ${DESIGN_FILE} ${PROJECT_SOURCE_DIR}/quasar.py ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseHeader.xslt Configuration.hxx_GENERATED validateDesign ${PROJECT_SOURCE_DIR}/Device/designToDeviceBaseBody.xslt
+	)	
+	
+	
+	
+	
 	set(DEVICEBASE_GENERATED_FILES
         include/DRoot.h
         src/DRoot.cpp
 	
+	generated/Base_DGFEXBoard.h
+	generated/Base_DGFEXBoard.cpp
+	
+	generated/Base_DBoardTemperatureSensors.h
+	generated/Base_DBoardTemperatureSensors.cpp
+	
 	)
 	
 	set(DEVICE_CLASSES
+	
+	src/DGFEXBoard.cpp
+	
+	src/DBoardTemperatureSensors.cpp
 	
 	)
 
